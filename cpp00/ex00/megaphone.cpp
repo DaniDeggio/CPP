cc5c14031f0c2de4ba0759a2103dfb0df0a6603a
Deggio <danideg12@gmail.com>
2024-05-21 16:30:28 +0200

    ex00 fatto

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: dde-giov <dde-giov@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 16:01:37 by dde-giov          #+#    #+#             */
-/*   Updated: 2024/05/21 16:03:55 by dde-giov         ###   ########.fr       */
+/*   Updated: 2024/05/21 16:23:27 by dde-giov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,17 @@
 
 int main(int argc, char **argv)
 {
-    if (argc == 1)
-        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-    else
-    argv++;
-    return (0);
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+	{
+		for (int i = 1; argv[i]; i++)
+		{
+			for (int j = 0; argv[i][j]; j++)
+				std::cout << (char)std::toupper(argv[i][j]);
+			std::cout << " ";
+		}
+	}
+	std::cout << std::endl;
+	return (0);
 }
