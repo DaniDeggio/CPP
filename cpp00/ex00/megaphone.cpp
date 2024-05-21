954d521bcfd40ad0368852360f0e1c5d6b523622
Deggio <danideg12@gmail.com>
2024-05-21 16:18:27 +0200

    iniziamo

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dde-giov <dde-giov@student.42roma.it>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/21 16:01:37 by dde-giov          #+#    #+#             */
+/*   Updated: 2024/05/21 16:03:55 by dde-giov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    if (argc == 1)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    else
+    argv++;
+    return (0);
+}
