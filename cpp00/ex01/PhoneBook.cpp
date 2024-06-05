b47b36f6b3cd95245a7157bf282c6ee5cd7bab6e
DaniDeggio <116410313+DaniDeggio@users.noreply.github.com>
2024-06-05 21:08:57 +0200

    Update PhoneBook.cpp Addcontact

--- cpp00/ex01/PhoneBook.cpp
+++ cpp00/ex01/PhoneBook.cpp
@@ -20,7 +20,20 @@ PhoneBook::~PhoneBook(){
     return ;
 }
 
-//void	PhoneBook::set_contact(std::string frist_name, std::string last_name,
-//	std::string nickname, std::string phone_number){
-//    static int  i;
+void PhoneBook::slide_contacts(PhoneBook pb){
+    int i = 7;
+
+    while(i > 0){
+        pb.contact[i] = pb.contact[i - 1]
+        i--;
+    }
+}
+
+void	PhoneBook::add_contact(std::string frist_name, std::string last_name,
+    std::string nickname, std::string phone_number, std::string secret){
+    Contact new;
+
+    PhoneBook::slide_contacts(this);
+    new.set_contat(frist_name, last_name, nickname, phone_nuber, secret);
+    this.contact[0] = new;
 //}
