f5128a2db92b972dde0b6f3e26b05fa52b5837ce
DaniDeggio <116410313+DaniDeggio@users.noreply.github.com>
2024-06-20 20:48:20 +0200

    Update main.cpp

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -20,11 +20,19 @@ int main(int ac, char **av){
 	std::string File(av[1]);
 	std::string s1(av[2]);
 	std::string s2(av[3]);
+	std::string line;
+	std::string File2 = File;
+	File2.append(".replace");
 
 	std::ifstream Input(File);
+	sts::ofsteeam Output(File2);
+
 	if (!Input.is_open()){
 		std::cout << "Error while opening the file: " << File << std::endl;
 		return (1);
 		}
-			
+	while (std::getline(Input, line)){
+		replacer(line, s1, s2);
+	}
+	input.close();
 }
