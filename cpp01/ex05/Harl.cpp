cb03fdd79118b404b2223a63ccc16b49623a8ec9
DaniDeggio <116410313+DaniDeggio@users.noreply.github.com>
2024-06-30 16:37:28 +0200

    Update Harl.cpp

--- cpp01/ex05/Harl.cpp
+++ cpp01/ex05/Harl.cpp
@@ -1,1 +1,8 @@
-#
+#include "Harl.hpp"
+
+void harl::debug(void){
+    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-
+ketchup burger. I really do!" << std::endl;
+}
+
+void 
