1dba2dcad9d3b241151de2e024a2639a9f57f4be
DaniDeggio <116410313+DaniDeggio@users.noreply.github.com>
2024-06-30 16:30:11 +0200

    Update Harl.hpp

--- cpp01/ex05/Harl.hpp
+++ cpp01/ex05/Harl.hpp
@@ -5,4 +5,14 @@
 #include <string>
 #include <iostream>
 
+class: Harl{
+    private:
+        void debug(void);
+        void info(void);
+        void warning(void);
+        void error(void);
+    public:
+        void complain(std::string level);
+};
+
 #endif
