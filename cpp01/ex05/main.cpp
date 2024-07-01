b6bb9cf44ea1736add0e5947430d21b7d68dd87f
Danilo De Giovanni <dde-giov@e3r4p2.42roma.it>
2024-07-01 14:39:20 +0200

    ex05 concluso

--- cpp01/ex05/main.cpp
+++ cpp01/ex05/main.cpp
@@ -1,29 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dde-giov <dde-giov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/01 14:18:24 by dde-giov          #+#    #+#             */
-/*   Updated: 2024/07/01 14:30:19 by dde-giov         ###   ########.fr       */
+/*   Created: 2024/07/01 14:18:01 by dde-giov          #+#    #+#             */
+/*   Updated: 2024/07/01 14:37:08 by dde-giov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef HARL_H
-# define HARL_H
+#include "Harl.hpp"
 
-#include <string>
-#include <iostream>
-
-class Harl{
-    private:
-        void debug(void);
-        void info(void);
-        void warning(void);
-        void error(void);
-    public:
-        void complain(std::string level);
-};
-
-#endif
+int main(int ac, char **av){
+	Harl test;
+	
+	if (ac == 1){
+		test.complain("DEBUG");
+		std::cout << std::endl;
+		test.complain("INFO");
+		std::cout << std::endl;
+		test.complain("WARNING");
+		std::cout << std::endl;
+		test.complain("ERROR");
+		std::cout << std::endl;
+	}
+	else {
+		test.complain(av[1]);
+	}
+}
