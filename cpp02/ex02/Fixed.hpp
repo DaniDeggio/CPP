5ef698610b6b7428e5eca55d89ffa5462bda690d
DaniDeggio <116410313+DaniDeggio@users.noreply.github.com>
2024-07-14 19:29:08 +0200

    Update Fixed.hpp

--- cpp02/ex02/Fixed.hpp
+++ cpp02/ex02/Fixed.hpp
@@ -34,9 +34,9 @@ class Fixed {
 		float toFloat( void ) const;
 		int toInt( void ) const;
 		static Fixed min(Fixed &n1, Fixed &n2);
-		static Fixed min(const Fixed &n1, const Fixed &n2);
+		static const Fixed min(const Fixed &n1, const Fixed &n2);
 		static Fixed max(Fixed &n1, Fixed &n2);
-		static Fixed max(const Fixed &n1, const Fixed &n2);
+		static const Fixed max(const Fixed &n1, const Fixed &n2);
 
 	// comparison operator
 	bool operator>(const Fixed &fx);
