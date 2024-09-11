1c6d1528d88b5aa22f699805657d2ba3a9535029
DanyDeggio <danideg12@gmail.com>
2024-09-11 17:14:00 +0200

    updated main

--- cpp03/ex00/main.cpp
+++ cpp03/ex00/main.cpp
@@ -6,23 +6,23 @@
 /*   By: dde-giov <dde-giov@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 14:18:01 by dde-giov          #+#    #+#             */
-/*   Updated: 2024/09/11 16:36:31 by dde-giov         ###   ########.fr       */
+/*   Updated: 2024/09/11 17:02:02 by dde-giov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 
 int main() {
-	ClapTrap claptrap("ClapTrap");
-	ClapTrap claptrap2("ClapTrap2");
+	ClapTrap claptrap("TestClap");
+	ClapTrap claptrap2("TestClap2");
 
 	for (int i = 0; i < 11; i++) {
-		claptrap.attack("ClapTrap2");
+		claptrap.attack("TestClap2");
 	}
 	claptrap2.takeDamage(42);
-	claptrap2.attack("ClapTrap");
+	claptrap2.attack("TestClap");
 	claptrap.beRepaired(42);
 	claptrap2.beRepaired(42);
-	claptrap2.attack("ClapTrap");
+	claptrap2.attack("TestClap");
 	return 0;
 }
--- cpp03/ex01/main.cpp
+++ cpp03/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dde-giov <dde-giov@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 14:18:01 by dde-giov          #+#    #+#             */
-/*   Updated: 2024/09/11 16:41:14 by dde-giov         ###   ########.fr       */
+/*   Updated: 2024/09/11 17:10:35 by dde-giov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,22 @@
 #include "ScavTrap.hpp"
 
 int main() {
-	ClapTrap claptrap("ClapTrap");
-	ClapTrap claptrap2("ClapTrap2");
+	ClapTrap claptrap("TestClap");
+	ClapTrap claptrap2("TestClap2");
+	ScavTrap scavtrap("TestScav");
+	ScavTrap scavtrap2("TestScav2");
 
 	for (int i = 0; i < 11; i++) {
-		claptrap.attack("ClapTrap2");
+		claptrap.attack("TestClap2");
 	}
 	claptrap2.takeDamage(42);
-	claptrap2.attack("ClapTrap");
+	claptrap2.attack("TestClap");
 	claptrap.beRepaired(42);
 	claptrap2.beRepaired(42);
-	claptrap2.attack("ClapTrap");
+	claptrap2.attack("TestClap");
+	scavtrap2.attack("TestClap");
+	claptrap.takeDamage(20);
+	claptrap.attack("TestScav");
+	scavtrap2.guardGate();
 	return 0;
 }
