1b138a4139b6b87566e8fe94fb648aafccd8e130
DanyDeggio <danideg12@gmail.com>
2024-09-11 20:28:42 +0200

    main added delte ex00 cpp04

--- cpp04/ex00/srcs/main.cpp
+++ cpp04/ex00/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dde-giov <dde-giov@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/11 19:54:02 by dde-giov          #+#    #+#             */
-/*   Updated: 2024/09/11 20:12:53 by dde-giov         ###   ########.fr       */
+/*   Updated: 2024/09/11 20:27:59 by dde-giov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,9 @@ int main()
 	i->makeSound(); //will output the cat sound!
 	j->makeSound();
 	meta->makeSound();
+
+	delete meta;
+	delete j;
+	delete i;
 	return 0;
 }
